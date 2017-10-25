df2f76eae316fff32a78506eff73ce6cfa0308b7
martonMeszaros <marton.m95@gmail.com>
2017-10-25 15:18:56 +0200

    LodePNG source and Ascii enum class

---
+++ Ascii.h
@@ -0,0 +1,20 @@
+//
+// Created by meszi on 2017.10.25..
+//
+
+#ifndef CPP_MAGIC_ASCII_ART_CREATOR_WASTEFUL_BOIS_ASCII_H
+#define CPP_MAGIC_ASCII_ART_CREATOR_WASTEFUL_BOIS_ASCII_H
+
+enum class Ascii : char {
+    BLACK       = '@',
+    CHARCOAL    = '#',
+    DARKGRAY    = '8',
+    MEDIUMGRAY  = '&',
+    MEDIUM      = 'o',
+    GRAY        = ':',
+    SLATEGRAY   = '*',
+    LIGHTGRAY   = '.',
+    WHITE       = ' '
+};
+
+#endif //CPP_MAGIC_ASCII_ART_CREATOR_WASTEFUL_BOIS_ASCII_H

d186a0affe24c5b2aac0f0cf850892d87f2531b1
martonMeszaros <marton.m95@gmail.com>
2017-10-26 13:39:50 +0200

    Doubled ascii ammount

--- Ascii.h
+++ Ascii.h
@@ -7,14 +7,22 @@
 
 // Not using enum class, because there is a type conflict when retrieving value
 namespace Ascii {
-    const char BLACK        = '@';
-    const char DARK_1       = '#';
-    const char DARK_2       = '8';
-    const char MEDIUM_DARK  = '&';
-    const char MEDIUM_LIGHT = 'o';
-    const char LIGHT_2      = '*';
-    const char LIGHT_1      = '.';
-    const char WHITE        = ' ';
+    const char BLACK    = '@';
+    const char DARK_1   = '#';
+    const char DARK_2   = '8';
+    const char DARK_3   = '9';
+    const char DARK_4   = '5';
+    const char DARK_5   = '3';
+    const char DARK_6   = 'C';
+    const char DARK_7   = 'H';
+    const char LIGHT_7  = '%';
+    const char LIGHT_6  = '=';
+    const char LIGHT_5  = '*';
+    const char LIGHT_4  = '^';
+    const char LIGHT_3  = ':';
+    const char LIGHT_2  = '-';
+    const char LIGHT_1  = '.';
+    const char WHITE    = ' ';
 }
 
 #endif //CPP_MAGIC_ASCII_ART_CREATOR_WASTEFUL_BOIS_ASCII_H
--- ImageParser.cpp
+++ ImageParser.cpp
@@ -5,14 +5,22 @@
 #include "ImageParser.h"
 
 const char ImageParser::selectCharacter(const int &lightness) {
-    const int threshold = 255 / 8;
-    if      (lightness < 1 * threshold) return Ascii::BLACK;
-    else if (lightness < 2 * threshold) return Ascii::DARK_1;
-    else if (lightness < 3 * threshold) return Ascii::DARK_2;
-    else if (lightness < 4 * threshold) return Ascii::MEDIUM_DARK;
-    else if (lightness < 5 * threshold) return Ascii::MEDIUM_LIGHT;
-    else if (lightness < 6 * threshold) return Ascii::LIGHT_2;
-    else if (lightness < 7 * threshold) return Ascii::LIGHT_1;
+    const int threshold = 255 / 16;
+    if      (lightness < 1 * threshold)  return Ascii::BLACK;
+    else if (lightness < 2 * threshold)  return Ascii::DARK_1;
+    else if (lightness < 3 * threshold)  return Ascii::DARK_2;
+    else if (lightness < 4 * threshold)  return Ascii::DARK_3;
+    else if (lightness < 5 * threshold)  return Ascii::DARK_4;
+    else if (lightness < 6 * threshold)  return Ascii::DARK_5;
+    else if (lightness < 7 * threshold)  return Ascii::DARK_6;
+    else if (lightness < 8 * threshold)  return Ascii::DARK_7;
+    else if (lightness < 9 * threshold)  return Ascii::LIGHT_7;
+    else if (lightness < 10 * threshold) return Ascii::LIGHT_6;
+    else if (lightness < 11 * threshold) return Ascii::LIGHT_5;
+    else if (lightness < 12 * threshold) return Ascii::LIGHT_4;
+    else if (lightness < 13 * threshold) return Ascii::LIGHT_3;
+    else if (lightness < 14 * threshold) return Ascii::LIGHT_2;
+    else if (lightness < 15 * threshold) return Ascii::LIGHT_1;
     return Ascii::WHITE;
 }
 

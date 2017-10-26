120c85651c59e16b182066f71f3c28f08b7faea7
martonMeszaros <marton.m95@gmail.com>
2017-10-26 18:10:58 +0200

    Udated ascii characters based on brightness

--- Ascii.h
+++ Ascii.h
@@ -8,19 +8,19 @@
 // Not using enum class, because there is a type conflict when retrieving value
 namespace Ascii {
     const char BLACK    = '@';
-    const char DARK_1   = '#';
-    const char DARK_2   = '8';
-    const char DARK_3   = '9';
-    const char DARK_4   = '5';
-    const char DARK_5   = '3';
-    const char DARK_6   = 'C';
-    const char DARK_7   = 'H';
-    const char LIGHT_7  = '%';
-    const char LIGHT_6  = '=';
+    const char DARK_1   = '0';
+    const char DARK_2   = 'G';
+    const char DARK_3   = '3';
+    const char DARK_4   = 'Y';
+    const char DARK_5   = '7';
+    const char DARK_6   = 'i';
+    const char DARK_7   = '?';
+    const char LIGHT_7  = '>';
+    const char LIGHT_6  = '+';
     const char LIGHT_5  = '*';
-    const char LIGHT_4  = '^';
+    const char LIGHT_4  = ';';
     const char LIGHT_3  = ':';
-    const char LIGHT_2  = '-';
+    const char LIGHT_2  = '"';
     const char LIGHT_1  = '.';
     const char WHITE    = ' ';
 }
